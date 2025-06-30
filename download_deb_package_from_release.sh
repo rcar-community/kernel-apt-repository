f33271af24b39cf0a9639ea48a2790ef60bdfc15
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-06-30 14:11:48 +0900

    Change to use release version package
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- download_deb_package_from_release.sh
+++ download_deb_package_from_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf kernel_deb_package.zip
-wget -q https://github.com/rcar-community/meta-sparrow-hawk/releases/download/dev-latest/kernel_deb_package.zip
+wget -q https://github.com/rcar-community/meta-sparrow-hawk/releases/download/v2025-06-30/kernel_deb_package.zip
 
