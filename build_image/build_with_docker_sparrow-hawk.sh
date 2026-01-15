cd7770f159078a62df0014e133f62333f1f8575f
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-01-15 12:59:34 +0900

    build_image: docker: Fix issue unshare fails due to permission
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build_image/build_with_docker_sparrow-hawk.sh
+++ build_image/build_with_docker_sparrow-hawk.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 cd ${SCRIPT_DIR}
 
 docker build -t debian-image-builder .
-docker run --rm -v $(pwd):/work debian-image-builder ./build_debian_for_sparrow-hawk.sh $@
+docker run --rm --cap-add SYS_ADMIN --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+    -v $(pwd):/work debian-image-builder ./build_debian_for_sparrow-hawk.sh $@
 
