8790db7902ab13f41a57827e00662e013ef6e5a4
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-01-22 11:18:55 +0900

    build_image: Introduce setup_yocto_bsp_variables.sh
    This script has common variable which is used by each build script.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

---
+++ build_image/setup_yocto_bsp_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REFERENCE=scarthgap-next
+RAW_GITHUB_BASE_URL=https://raw.githubusercontent.com/rcar-community/meta-sparrow-hawk/refs
+# Check tag(vYYYY-MM-DD) or branch
+if [[ "$(echo ${REFERENCE} | grep -E 'v[0-9]{4}-[0-9]{2}-[0-9]{2}')" == "" ]]; then
+    YOCTO_BSP_RAW_GITHUB_URL=${RAW_GITHUB_BASE_URL}/heads/${REFERENCE}
+else
+    YOCTO_BSP_RAW_GITHUB_URL=${RAW_GITHUB_BASE_URL}/tags/${REFERENCE}
+fi
+unset RAW_GITHUB_BASE_URL
+
